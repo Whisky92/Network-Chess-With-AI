4867193c8e9026c06c0e324610b39d725d13a63b
Whisky92 <olah.tomi.92@gmail.com>
2023-03-11 19:06:38 +0100

    Updated message box designs

--- gui/message_box.py
+++ gui/message_box.py
@@ -1,6 +1,7 @@
 from my_message_box import MyDualMessageBox, MySingleMessageBox, MyQuadrupleMessageBox
 from model.piece_type import PieceType
 from PyQt5.QtGui import QPixmap
+from PyQt5 import QtCore
 
 
 class MessageBox:
@@ -11,6 +12,8 @@ class MessageBox:
         msgbox = MyDualMessageBox("The result of the game is stalemate",
                                   b1_text="Start a new game",
                                   b2_text="Return to main menu")
+        msgbox.questionLabel.setAlignment(QtCore.Qt.AlignCenter)
+
         msgbox.exec_()
 
     @staticmethod
@@ -21,6 +24,8 @@ class MessageBox:
         msgbox = MyDualMessageBox("CheckMate!\nThe result of the game is: " + game_result,
                                   b1_text="Start a new game",
                                   b2_text="Return to main menu")
+        msgbox.questionLabel.setAlignment(QtCore.Qt.AlignCenter)
+
         msgbox.exec_()
 
     @staticmethod
@@ -32,51 +37,60 @@ class MessageBox:
         msgbox = MyDualMessageBox("The result of the game is: " + game_result,
                                   b1_text="Start a new game",
                                   b2_text="Return to main menu")
+        msgbox.questionLabel.setAlignment(QtCore.Qt.AlignCenter)
 
         msgbox.btn1.clicked.connect(lambda: MessageBox.__start_new_game(game, msgbox))
         msgbox.btn2.clicked.connect(lambda: MessageBox.__return_to_main_menu(game.widget, msgbox))
+
         msgbox.exec_()
 
     @staticmethod
     def make_enemy_surrender_message_box(game, player_name, enemy_player_name):
 
         msgbox = MyDualMessageBox("Do " + enemy_player_name + " want to surrender?")
+        msgbox.questionLabel.setAlignment(QtCore.Qt.AlignCenter)
 
         def on_btn1_click():
             msgbox.close()
             MessageBox.end_game_message_box(game, player_name)
 
         msgbox.btn1.clicked.connect(on_btn1_click)
+
         msgbox.exec_()
 
     @staticmethod
     def draw_request_message_box(game, enemy_player_name):
 
         msgbox = MyDualMessageBox("Do " + enemy_player_name + " agree with a draw?")
+        msgbox.questionLabel.setAlignment(QtCore.Qt.AlignCenter)
 
         def on_btn1_click():
             msgbox.close()
             MessageBox.end_game_message_box(game, "Draw")
 
         msgbox.btn1.clicked.connect(on_btn1_click)
+
         msgbox.exec_()
 
     @staticmethod
     def surrender_message_box(game, enemy_player_name):
 
         msgbox = MyDualMessageBox("Do you really want to surrender?")
+        msgbox.questionLabel.setAlignment(QtCore.Qt.AlignCenter)
 
         def on_btn1_click():
             msgbox.close()
             MessageBox.end_game_message_box(game, enemy_player_name)
 
         msgbox.btn1.clicked.connect(on_btn1_click)
+
         msgbox.exec_()
 
     @staticmethod
     def still_check_message_box():
 
         msgbox = MySingleMessageBox("Your king is in check.\nYou can't step with this piece.")
+        msgbox.questionLabel.setAlignment(QtCore.Qt.AlignCenter)
 
         msgbox.exec_()
 
@@ -87,6 +101,7 @@ class MessageBox:
             else game.player2Name.text()
 
         msgbox = MySingleMessageBox(name + "!\nYour king is in check.")
+        msgbox.questionLabel.setAlignment(QtCore.Qt.AlignCenter)
 
         msgbox.exec_()
 
@@ -94,6 +109,7 @@ class MessageBox:
     def promote_message_box(game, cell, gui_cell):
 
         msgbox = MyQuadrupleMessageBox("Choose the type of piece you would like to promote the pawn to")
+        msgbox.questionLabel.setAlignment(QtCore.Qt.AlignCenter)
 
         def on_btn1_clicked():
 
